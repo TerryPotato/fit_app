b28374d61bbc936dcab0d57df2c20a9ee9665176
Pamela Prados <pamgendrop@gmail.com>
2026-01-31 10:51:29 -0600

    clase

--- components/ImageSlider.jsx
+++ components/ImageSlider.jsx
@@ -1,25 +1,55 @@
-import { Text } from "react-native";
-import { widthPercentageToDP as wp } from "react-native-responsive-screen";
-import Carousel from "react-native-snap-carousel";
+import { Image, View } from "react-native";
+import Carousel from "react-native-reanimated-carousel";
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from "react-native-responsive-screen";
 import { sliderImages } from "../constants";
 
 export default function ImageSlider() {
+  const width = wp(100) - 70;
   return (
-    <Carousel
-      data={sliderImages}
-      loop={true}
-      autoplay={true}
-      renderItem={ItemCard}
-      hasParallaxImages={true}
-      sliderWidth={wp(100)}
-      firstItem={1}
-      autoplayInterval={400}
-      itemWidth={wp(100) - 70}
-      slideStyle={{ display: "flex", alignItems: "center" }}
-    />
+    <View style={{ width: wp(100), alignItems: "center" }}>
+      <Carousel
+        loop
+        width={width}
+        height={hp(25)}
+        autoPlay={true}
+        data={sliderImages}
+        autoPlayInterval={4000}
+        renderItem={({ item, index, animationValue }) => (
+          <ItemCard item={item} index={index} animationValue={animationValue} />
+        )}
+      />
+    </View>
   );
 }
 
-const ItemCard = () => {
-  return <Text>Slide</Text>;
+const ItemCard = ({ item, index, animationValue }) => {
+  const parallaxStyle = {
+    transform: [
+      {
+        scale: animationValue.interpolate({
+          inputRange: [-1, 0, 1],
+          outputRange: [0.9, 1, 0.9],
+        }),
+      },
+    ],
+  };
+  return (
+    <View style={{ width: wp(100) - 70, height: hp(25) }}>
+      <Image
+        source={item}
+        style={[
+          {
+            width: "100%",
+            height: "100%",
+            borderRadius: 30,
+            resizeMode: "contain",
+          },
+          parallaxStyle,
+        ]}
+      />
+    </View>
+  );
 };
